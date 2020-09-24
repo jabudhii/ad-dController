7aee4036273937a3ebce117bf5604846f2896da0
Matt Kimball <jabudhii@yahoo.com>
2020-09-24 13:54:09 -0400

    b732
    further improvements to accessing and editing player data

---
+++ ad&dController/ButtonStylesFlat.swift
@@ -0,0 +1,45 @@
+//
+//  ButtonStylesFlat.swift
+//  ad&dController
+//
+//  Created by Matt Kimball on 9/24/20.
+//  Copyright © 2020 matthewKimball. All rights reserved.
+//
+
+import SwiftUI
+
+struct ButtonStylesFlat: View {
+    var option: String
+    var body: some View {
+        switch(option){
+        case "write":
+            HStack{
+                Image(systemName: "pencil.circle.fill").imageScale(.large).foregroundColor(.white)
+                Text("Write").foregroundColor(.white)
+            }.padding().background(Color.green).cornerRadius(75)
+        case "clear":
+            HStack{
+                Image(systemName: "chevron.left.circle.fill").imageScale(.large).foregroundColor(.white)
+                Text("Clear").foregroundColor(.white)
+            }.padding().background(Color.blue).cornerRadius(75)
+        case "remove":
+            HStack{
+                Image(systemName: "x.circle.fill").imageScale(.large).foregroundColor(.white)
+                Text("Remove").foregroundColor(.white)
+            }.padding().background(Color.red).cornerRadius(75)
+        default:
+            HStack{
+                Image(systemName: "x.circle.fill").imageScale(.large).foregroundColor(.white)
+                Text("ERROR").foregroundColor(.white)
+            }.padding().background(Color.orange).cornerRadius(75)
+        }
+    }
+}
+
+
+
+struct ButtonStylesFlat_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStylesFlat(option: "write")
+    }
+}
--- ad&dController/views/notesViewSubviews/NotesPlayerView.swift
+++ ad&dController/views/notesViewSubviews/NotesPlayerView.swift
@@ -14,33 +14,92 @@ struct NotesPlayerView: View {
     @State var players:[NSManagedObject] = []
     @State var newPlayerName = ""
     @State var newPlayerNote = ""
+    @State var showSheet = false
     
     var body: some View {
         VStack{
             HStack{
                 VStack{
-                    TextField("Name", text: $newPlayerName).padding().background(Color.white).border(Color.black, width: 4).cornerRadius(5)
-                    TextField("Name", text: $newPlayerNote).padding().background(Color.white).border(Color.black, width: 4).cornerRadius(5)
-                    Button(action:{
-                        if((self.newPlayerName == "") || (self.newPlayerNote == "")){
-                            
-                        }  // END NOT GOOD
-                        else{
-                            self.addNewPlayer()
+                    HStack{
+                        Text("Name:")
+                        TextField("Name", text: $newPlayerName)
+                    }.padding().background(Color.white).border(Color.black, width: 4).cornerRadius(5).foregroundColor(.black)
+                    HStack{
+                        Text("Note:")
+                        TextField("Name", text: $newPlayerNote)
+                    }.padding().background(Color.white).border(Color.black, width: 4).cornerRadius(5).foregroundColor(.black)
+                    HStack{
+                        Button(action:{
+                            if((self.newPlayerName == "") || (self.newPlayerNote == "")){
+                                
+                            }  // END NOT GOOD
+                            else{
+                                self.addNewPlayer()
+                                self.newPlayerName = ""
+                                self.newPlayerNote = ""
+                            }  // END GOOD
+                        }){
+                            ButtonStylesFlat(option: "write")
+                        }  // END BUTTON
+                        Spacer()
+                        Button(action:{
                             self.newPlayerName = ""
                             self.newPlayerNote = ""
-                        }  // END GOOD
-                    }){
-                        Text("Add Player")
-                    }  // END BUTTON
+                        }){
+                            ButtonStylesFlat(option: "clear")
+                        }  // END BUTTON
+                    }
                 }  // END VSTACK
                 Spacer()
                 ScrollView{
+                    Text("Players").font(.largeTitle).bold()
                     ForEach(players, id:\.self){ thisPlayer in
-                        VStack{
+                        Button(action: {
+                            newPlayerName = (thisPlayer as? CorePlayers)?.name ?? "Error"
+                            newPlayerNote = (thisPlayer as? CorePlayers)?.note ?? "Error"
+                                self.showSheet = true
+                            
+                        }){
                             Text((thisPlayer as? CorePlayers)?.name ?? "Error").bold()
                             Text((thisPlayer as? CorePlayers)?.note ?? "Error")
-                        }  // END VSTACK
+                        }.buttonStyle(PlainButtonStyle()).sheet(isPresented: self.$showSheet, content: {
+                            VStack{
+                                TextField("Update Name", text: $newPlayerName).padding().background(Color.white).border(Color.black, width: 4).cornerRadius(5).foregroundColor(.black)
+                                TextField("Update Notes", text: $newPlayerNote).padding().background(Color.white).border(Color.black, width: 4).cornerRadius(5).foregroundColor(.black)
+                                Spacer()
+                                Text("Warning:").bold()
+                                Text("This will delete, alter, or otherwise modify the data stored in this application.\nPlease ensure you have properly considered the results of the changes you are performing before selecting any button.").multilineTextAlignment(.center)
+                                Spacer()
+                                HStack{
+                                    Button(action:{
+                                        if((self.newPlayerName == "") || (self.newPlayerNote == "")){
+                                            
+                                        }  // END NOT GOOD
+                                        else{
+                                            self.addNewPlayer()
+                                            self.newPlayerName = ""
+                                            self.newPlayerNote = ""
+                                        }  // END GOOD
+                                    }){
+                                        ButtonStylesFlat(option: "write")
+                                    }  // END UPDATE BUTTON
+                                    Spacer()
+                                    Button(action:{
+                                        self.newPlayerName = ""
+                                        self.newPlayerNote = ""
+                                    }){
+                                        ButtonStylesFlat(option: "clear")
+                                    }  // END BUTTON
+                                    Spacer()
+                                    Button(action:{
+                                        self.deletePlayer(thisPlayerName: newPlayerName)
+                                        self.showSheet = false
+                                    }){
+                                        ButtonStylesFlat(option: "remove")
+                                    }  // END DELETEBUTTON
+                                }  // END HSTACK
+                            }.padding()  // END VSTACK
+                        })
                     }  // END FOR EACH
                 }.navigationTitle("Players").padding().background(Color.gray).cornerRadius(25)  // END SCROLL
             }
@@ -89,6 +148,102 @@ struct NotesPlayerView: View {
         }
     }  // END loadPlayers
     
+    
+    func updatePlayerName(currentPlayerName:String, newPlayerName:String){
+        print("currentPlayerName = \(currentPlayerName) --- changing to = \(newPlayerName)")
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "CorePlayers")
+        fetchRequest.predicate = NSPredicate(format: "name = %@", currentPlayerName)
+        
+        do {
+            let fetchReturn = try managedContext.fetch(fetchRequest)
+            
+            let objectUpdate = fetchReturn[0] as! NSManagedObject
+            objectUpdate.setValue(newPlayerName, forKey: "name")
+            do {
+                try managedContext.save()
+                print("updated successfully")
+                self.loadPlayers()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }  // END updatePlayerName
+    
+    
+    func updatePlayerNote(currentPlayerNote:String, newPlayerNote:String){
+        print("currentPlayerNote = \(currentPlayerNote) --- changing to = \(newPlayerNote)")
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "CorePlayers")
+        fetchRequest.predicate = NSPredicate(format: "note = %@", currentPlayerNote)
+        
+        do {
+            let fetchReturn = try managedContext.fetch(fetchRequest)
+            
+            let objectUpdate = fetchReturn[0] as! NSManagedObject
+            objectUpdate.setValue(newPlayerNote, forKey: "note")
+            do {
+                try managedContext.save()
+                print("updated successfully")
+                self.loadPlayers()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }  // END updatePlayerName
+    
+    //Delete
+    func deletePlayer(thisPlayerName:String){
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "CorePlayers")
+        fetchRequest.predicate = NSPredicate(format: "name = %@", thisPlayerName)
+        
+        do {
+            let fetchReturn = try managedContext.fetch(fetchRequest)
+            
+            let objectDelete = fetchReturn[0] as! NSManagedObject
+            managedContext.delete(objectDelete)
+            do {
+                try managedContext.save()
+                print("deleted successfully")
+                self.loadPlayers()
+            } catch let error as NSError {
+                print("Could not delete. \(error), \(error.userInfo)")
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }  // END deletePlayer
     
     
     
